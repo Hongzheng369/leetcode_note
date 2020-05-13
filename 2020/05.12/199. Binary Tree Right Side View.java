f3f20b9d386c41a53ecf799f307f7d48fc939fce
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-05-12 20:12:46 -0500

    first_commit

---
+++ 2020/05.12/199. Binary Tree Right Side View.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res, 0);
+        return res;
+    }
+    public void helper(TreeNode root, List<Integer> res, int curDepth){
+        if(root == null)
+            return;
+        if(curDepth == res.size())
+            res.add(root.val);
+        helper(root.right, res, curDepth + 1);
+        helper(root.left, res, curDepth + 1);
+    }
+}
