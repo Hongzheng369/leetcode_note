525e0b2dd5b7b3a9292d76884ca7155757aabb14
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-05-12 20:22:40 -0500

    first_commit

--- 2020/05.12/415. Add Strings.java
+++ 2020/05.12/415. Add Strings.java
@@ -1,13 +1,24 @@
-public class Solution {
-    public String addStrings(String num1, String num2) {
+class Solution {
+    public String addStrings(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int carry = 0;
-        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
-            int x = i < 0 ? 0 : num1.charAt(i) - '0';
-            int y = j < 0 ? 0 : num2.charAt(j) - '0';
-            sb.append((x + y + carry) % 10);
-            carry = (x + y + carry) / 10;
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0){
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if(j >= 0){
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            sb.append(sum % 10);
+            carry = sum/ 10;
+            
+            
         }
+        if(carry != 0)
+            sb.append(carry);
         return sb.reverse().toString();
     }
 }
