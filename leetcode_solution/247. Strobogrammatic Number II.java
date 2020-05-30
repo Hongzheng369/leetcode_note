43773b290bdb1d4a23576e36f349094af67bd7f1
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-05-30 17:35:40 -0500

    update

---
+++ leetcode_solution/247. Strobogrammatic Number II.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<String> findStrobogrammatic(int n) {
+        return helper(n, n);
+    }
+    public List<String> helper(int targetLen, int totalLen) {
+        if (targetLen == 0) {
+            return new ArrayList(Arrays.asList(""));
+        }
+        if (targetLen == 1) {
+            return new ArrayList(Arrays.asList("1", "8", "0"));
+        }
+        List<String> sub = helper(targetLen - 2, totalLen);
+        List<String> result = new ArrayList<>();
+        for (String str : sub) {
+            if (targetLen != totalLen) {
+                result.add("0" + str + "0");
+            }
+            result.add("1" + str + "1");
+            result.add("6" + str + "9");
+            result.add("9" + str + "6");
+            result.add("8" + str + "8");
+        }
+        return result;
+    }
+}
