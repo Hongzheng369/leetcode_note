35a0e90580405495e11f1e7509b40bf6fb37f0f2
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-01 16:41:04 -0500

    update

--- 2020/06.01/143. Reorder List.java
+++ 2020/06.01/143. Reorder List.java
@@ -11,8 +11,8 @@ class Solution {
             if(head == null || head.next == null) return;
             
             //Find the middle of the list
-            ListNode p1=head;
-            ListNode p2=head;
+            ListNode p1 = head;
+            ListNode p2 = head;
             while(p2.next != null && p2.next.next != null){ 
                 p1 = p1.next;
                 p2 = p2.next.next;
@@ -33,7 +33,7 @@ class Solution {
             p1 = head;
             p2 = preMiddle.next;
             while(p1 != preMiddle){
-                
+
                 preMiddle.next = p2.next;
                 p2.next = p1.next;
                 p1.next = p2;
