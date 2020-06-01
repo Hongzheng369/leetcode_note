fc5e62a434db7d86364872b9f8098cfa1c7b975a
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-01 16:40:45 -0500

    update

--- 2020/06.01/143. Reorder List.java
+++ 2020/06.01/143. Reorder List.java
@@ -22,6 +22,7 @@ class Solution {
             ListNode preMiddle = p1;
             ListNode preCurrent = p1.next;
             while(preCurrent.next != null){
+
                 ListNode current = preCurrent.next;
                 preCurrent.next = current.next;
                 current.next = preMiddle.next;
@@ -32,6 +33,7 @@ class Solution {
             p1 = head;
             p2 = preMiddle.next;
             while(p1 != preMiddle){
+                
                 preMiddle.next = p2.next;
                 p2.next = p1.next;
                 p1.next = p2;
