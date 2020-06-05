1eadacbdda87a2f40af9d0fa29256eb5c3a77479
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-05 11:01:42 -0500

    initial

--- 2020/06.05/270. Closest Binary Search Tree Value.java
+++ 2020/06.05/270. Closest Binary Search Tree Value.java
@@ -24,3 +24,18 @@ Output: 4
 所以博主最先想到用中序遍历来做，一个一个的比较，维护一个最小值，不停的更新，
 实际上这种方法并没有提高效率，用其他的遍历方法也可以，参见代码如下：
 */
+
+class Solution {
+
+    public int closestValue(TreeNode root, double target) {
+        int res = root.val;
+        if (target < root.val && root.left) {
+            int l = closestValue(root.left, target);
+            if (abs(res - target) >= abs(l - target)) res = l;
+        } else if (target > root.val && root.right) {
+            int r = closestValue(root.right, target);
+            if (abs(res - target) >= abs(r - target)) res = r;
+        }
+        return res;
+    }
+}
