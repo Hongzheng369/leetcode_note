22ce6c9da971ea7ee7b09f6e6ce4cda30d93edb1
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-07 11:11:32 -0500

    update

--- 2020/06.06/1027. Longest Arithmetic Sequence.java
+++ 2020/06.06/1027. Longest Arithmetic Sequence.java
@@ -25,3 +25,19 @@ class Solution {
         return 1+max;
     }
 }
+
+class Solution {
+    public int longestArithSeqLength(int[] A) {
+        int res = 2, n = A.length;
+        HashMap<Integer, Integer>[] dp = new HashMap[n];
+        for (int j = 0; j < A.length; j++) {
+            dp[j] = new HashMap<>();
+            for (int i = 0; i < j; i++) {
+                int d = A[j] - A[i];
+                dp[j].put(d, dp[i].getOrDefault(d, 1) + 1);
+                res = Math.max(res, dp[j].get(d));
+            }
+        }
+        return res;
+    }
+}
