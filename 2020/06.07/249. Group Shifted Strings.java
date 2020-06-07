aa61f698849bf81232cbcb4be1a690f58d2d34f1
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-07 17:21:21 -0500

    update

--- 2020/06.07/249. Group Shifted Strings.java
+++ 2020/06.07/249. Group Shifted Strings.java
@@ -1,3 +1,23 @@
+/*
+Given a string, we can "shift" each of its letter to its successive letter, 
+for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence:
+"abc" -> "bcd" -> ... -> "xyz"
+
+
+Given a list of strings which contains only lowercase alphabets, 
+group all strings that belong to the same shifting sequence.
+For example, given: ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"], Return:
+[
+  ["abc","bcd","xyz"],
+  ["az","ba"],
+  ["acef"],
+  ["a","z"]
+]
+Note: For the return value, each inner list's elements must follow the lexicographic
+ order.
+things to consider: 1 does each group allow duplicates, such as ["a", "a"]. 
+2 how to calculate unique hash key.
+*/
 public class Solution {
     public List<List<String>> groupStrings(String[] strings) {
         Map<String, List<String>> map = new HashMap<>();
