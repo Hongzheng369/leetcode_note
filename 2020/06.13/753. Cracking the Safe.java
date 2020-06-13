b93d34f1ec894243634fe2d1a8f64a387b4d02df
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-13 17:08:27 -0500

    initial

--- 2020/06.13/753. Cracking the Safe.java
+++ 2020/06.13/753. Cracking the Safe.java
@@ -1,33 +1,32 @@
 class Solution {
     public String crackSafe(int n, int k) {
-        int keys = (int)Math.pow(k, n);
+        int max = (int)Math.pow(k,n);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             sb.append(0);
         }
         Set<String> set = new HashSet<>();
         set.add(sb.toString());
-        dfs(set, sb, keys, n, k);
+        dfs(set, sb, max, n, k);
         return sb.toString();
     }
-    private boolean dfs(Set<String> set, StringBuilder sb, int keys, int n, int k) {
-        if (set.size() == keys) {
-            return true;
-        }
-        String prefix = sb.substring(sb.length() - n + 1, sb.length());
-        for (int i = 0; i < k; i++) {
+    
+    public boolean dfs(Set<String> set, StringBuilder sb, int max, int n, int k){
+        if(set.size() == max) return true;
+        String prefix = sb.substring(sb.length() - (n - 1), sb.length());
+        for(int i = 0; i < k; i++){
             String test = prefix + i;
-            if (!set.contains(test)) {
-                sb.append(i);
+            if(!set.contains(test)){
                 set.add(test);
-                if (dfs(set, sb, keys, n, k)) {
+                sb.append(i);
+                if(dfs(set, sb, max, n, k))
                     return true;
-                } else {
+                else{
                     sb.deleteCharAt(sb.length() - 1);
                     set.remove(test);
-                }
+                }                
             }
         }
         return false;
     }
-} 
+}
