70bc9dc8948a71330aafd4a3fa1c0b57e33f3d05
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-15 13:34:03 -0500

    update

--- 2020/06.14/273. Integer to English Words.java
+++ 2020/06.14/273. Integer to English Words.java
@@ -1,28 +1,26 @@
 class Solution {
-    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+       private final String[] less_20 = {"","One", "Two", "Three", "Four", "Five","Six", "Seven", "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    private final String[] tens = {"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    private final String[] thousands = {"","Thousand","Million","Billion"};
     
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
         int i = 0;
-        String words = "";
-        while( num > 0){
+        String word = "";
+        while(num > 0){
             if(num % 1000 != 0){
-                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+                word = helper(num % 1000) + thousands[i] + " " + word;
             }
             num /= 1000;
             i++;
         }
-        return words.trim();
+        return word.trim();
     }
+    
     public String helper(int num){
         if(num == 0) return "";
-        else if(num < 20)
-            return LESS_THAN_20[num] + " ";
-        else if(num < 100)
-            return TENS[num/10] + " " + helper(num%10);
-        else 
-            return LESS_THAN_20[num/100] + " Hundred " + helper(num % 100);
+        else if(num < 20) return less_20[num] + " ";
+        else if(num < 100) return tens[num / 10] + " " + helper(num % 10);
+        else return less_20[num / 100] + " Hundred " + helper(num % 100);
     }
 }
