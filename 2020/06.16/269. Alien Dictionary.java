f1f784b45f883bf22569025b86e4e26f96231863
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-16 15:03:55 -0500

    initial

---
+++ 2020/06.16/269. Alien Dictionary.java
@@ -0,0 +1,48 @@
+Class Solution{
+    public String alienDic(String[] words){
+        StringBuilder sb = new StringBuilder();
+        HashMap<Character, HashSet<Character>> map = new HashMap<>();
+        HashMap<Character, Character> degree = new HashMap<>();
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < words[i].length(); j++){
+                map.put(words[i].charAt(j), 0);
+            }
+        }
+        for(int i = 0; i < words.length - 1; i++){
+            char[] c1 = words[i].toCharArray();
+            char[] c2 = words[i+1].toCharArray();
+            int len = Math.min(c1.length, c2.length);
+            for(int j = 0; j < len; j++){
+                if(c1[j] != c2[j]){
+                    HashSet<Character> set = new HashSet<>();
+                    if(map.containsKey(c1))
+                        set = map.get(c1);
+                    if(!set.contains(c2)){
+                        set.add(c2[j])
+                        map.put(c1[j],set);
+                        degree.put(c2[j], degree.get(c2[j]) + 1);  
+                    }
+                    break;
+                }
+                //if()
+            }
+        }
+        Queue<Character> queue = new Queue<>();
+        for(char c: degree.keySet()){
+            if(degree.get(c) == 0)  queue.add(c);
+        }
+        while(!queue.isEmpty()){
+            char c1 = queue.remove();
+            sb.append(c1);
+            //for(char)
+            if(map.containsKey(c1)){
+                for(char c: map.get(c1)){
+                    degree.put(c, degree.get(c) - 1);
+                    if(degree.get(c) == 0) queue.add(c);
+                }
+            }
+        }
+        if(sb.length() != degree.size()) return "";
+        return sb.toString();
+    }
+}
