cdc9022b4a9fd607fbcec23b97167eb7df840fa7
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-17 17:18:04 -0500

    update

--- 2020/06.17/438. Find All Anagrams in a String.java
+++ 2020/06.17/438. Find All Anagrams in a String.java
@@ -29,3 +29,38 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int left = 0;
+        int right = 0;
+        int sLen = s.length();
+        int pLen = p.length();
+        int[] hash = new int[256];
+        List<Integer> pos = new ArrayList<Integer>();
+        
+        for (int i = 0; i<pLen; i++) {
+            hash[(int)p.charAt(i)]++;
+        }
+        int count = 0;
+        
+        while (right < sLen) {
+            if (hash[s.charAt(right)] > 0) {
+                hash[s.charAt(right)]--;
+                count++;
+                right++;
+            } else {
+                hash[s.charAt(left)]++;
+                count--;
+                left++;
+            }
+
+            if(count == pLen) {
+                pos.add(left);
+            }
+            
+        }
+        return pos;
+    }
+}
