da107129d6014360b6dc17e0df36ec57c313f62e
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-21 13:59:26 -0500

    update

--- 2020/06.21/139. Word Break.java
+++ 2020/06.21/139. Word Break.java
@@ -3,10 +3,10 @@ class Solution {
         Set<String> set = new HashSet<>(wordDict);
         return helper(s, set);
     }
+    
     private boolean helper(String s, Set<String> set){
-        int len = s.length();
-        if(len == 0)    return true;
-        for(int i = 0; i <= len; i++){
+        if(s.length() == 0)    return true;
+        for(int i = 0; i <= s.length(); i++){
             if(set.contains(s.substring(0, i)) && helper(s.substring(i), set))  return true;
         }
         return false;
