3a4a09f43c6369683006e09498f6eca8cae19a23
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-21 20:40:50 -0500

    update

--- 2020/06.21/621. Task Scheduler.java
+++ 2020/06.21/621. Task Scheduler.java
@@ -1,25 +1,23 @@
-public class Solution {
+class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] counter = new int[26];
-        int max = 0;
-        int maxCount = 0;
-        for(char task : tasks) {
-            counter[task - 'A']++;
-            if(max == counter[task - 'A']) {
-                maxCount++;
-            }
-            else if(max < counter[task - 'A']) {
-                max = counter[task - 'A'];
-                maxCount = 1;
-            }
+
+        int[] frequencies = new int[26];
+        for (char c : tasks) frequencies[c - 'A']++;
+
+        Queue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        for (int i : frequencies) if (i != 0) pq.add(i);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int time = 0, tasksRemaining = tasks.length;
+        while (tasksRemaining > 0) {
+            Integer prev = map.get(time);
+            if (prev != null) pq.add(prev); 
+            Integer cur = pq.poll();
+            if (cur != null) tasksRemaining--;
+            if (cur != null && --cur > 0) map.put(time + n + 1, cur);
+            time++;
         }
-        
-        int partCount = max - 1;
-        int partLength = n - (maxCount - 1);
-        int emptySlots = partCount * partLength;
-        int availableTasks = tasks.length - max * maxCount;
-        int idles = Math.max(0, emptySlots - availableTasks);
-        
-        return tasks.length + idles;
+        return time;
+
     }
-}   
+}
