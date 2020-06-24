d07d87c74386806f4ae182ff1f5b24ba5667dbdb
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-23 22:13:34 -0500

    update

---
+++ 2020/06.23/173. Binary Search Tree Iterator.java
@@ -0,0 +1,21 @@
+class Solution{
+    Stack<TreeNode> stack;
+    TreeNode node;
+    public BSTIterator(TreeNode root){
+        stack = new Stack();
+        node = root;
+    }
+    public boolean hasNext(){
+        return !stack.isEmpty() || node != null;
+    }
+    public int next(){
+        while(node != null){
+            stack.push(node);
+            node = node.left;
+        }
+        node = stack.pop();
+        int res = node.val;
+        node = node.right;
+        return res;
+    }
+}
