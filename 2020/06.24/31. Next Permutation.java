6cdd3b1a4a93389d6ef7b1af9b9b1f09bb8812a0
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-24 14:32:50 -0500

    update

--- 2020/06.24/31. Next Permutation.java
+++ 2020/06.24/31. Next Permutation.java
@@ -1,28 +1,27 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        while(i>=0 && nums[i] >= nums[i+1] )
+        while(i >= 0 && nums[i] >= nums[i + 1])
             i--;
-        if(i>=0){
+        if(i >= 0){
             int j = nums.length - 1;
             while(j > 0 && nums[j] <= nums[i])
                 j--;
-            swap(nums,i,j);
+            swap(nums, i, j);
         }
-        reverse(nums,i+1);
+        reverse(nums, i + 1);
     }
-    
     public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
+        int tmp = nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
+        nums[j] = tmp;
     }
     public void reverse(int[] nums, int start){
-        int i = start, j = nums.length -1 ;
-        while(i<j){
-            swap(nums, i ,j);
+        int i = start, j = nums.length - 1;
+        while(i < j){
+            swap(nums, i, j);
             i++;
             j--;
         }
-    }    
+    }
 }
