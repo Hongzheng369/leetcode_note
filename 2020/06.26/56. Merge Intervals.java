7d81e9d632cb814cf280bf314411f260e29ce1a4
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-26 22:23:19 -0500

    initial

---
+++ 2020/06.26/56. Merge Intervals.java
@@ -0,0 +1,22 @@
+class Solution{
+    public int[][] merge(int[][] intevals){
+        //List<int[]> res = new ArrayList<>();
+        int n = intevals.length;
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for(int i = 0; i < n; i++){
+            start[i] = intevals[i][0];
+            end[i] = intevals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        List<int[]> res = new ArrayList<>();
+        for(int i = 0, j = 0; i < n; i++){
+            if(i == n - 1 || start[i + 1] > end[i]){
+                res.add(new int[]{start, end});
+                j = i + 1;
+            }
+        }
+        return res.toArray(new int[res.size()][2]);
+    }
+}
