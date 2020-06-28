168ed5553ada34f840ba6c04e9df98d873a069b0
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-06-27 22:44:17 -0500

    update

--- 2020/06.27/721. Accounts Merge.java
+++ 2020/06.27/721. Accounts Merge.java
@@ -1,33 +1,32 @@
-class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+class Solution{
+    public List<List<String>> accountsMerge(List<List<String>> accounts){
         Map<String, String> emailToName = new HashMap();
         Map<String, ArrayList<String>> graph = new HashMap();
-        for (List<String> account: accounts) {
+        for(List<String> account: accounts){
             String name = "";
-            for (String email: account) {
-                if (name == "") {
+            for(String email: account){
+                if(name == ""){
                     name = email;
                     continue;
                 }
-                graph.computeIfAbsent(email, x-> new ArrayList<String>()).add(account.get(1));
-                graph.computeIfAbsent(account.get(1), x-> new ArrayList<String>()).add(email);
+                graph.computeIfAbsent(email, x -> new ArrayList<String>()).add(account.get(1));
+                graph.computeIfAbsent(account.get(1), x -> new ArrayList<String>()).add(email);
                 emailToName.put(email, name);
             }
         }
-
         Set<String> seen = new HashSet();
-        List<List<String>> ans = new ArrayList();
-        for (String email: graph.keySet()) {
-            if (!seen.contains(email)) {
+        List<List<String>> res = new ArrayList();
+        for(String email: graph.keySet()){
+            if(!seen.contains(email)){
                 seen.add(email);
                 Stack<String> stack = new Stack();
                 stack.push(email);
                 List<String> component = new ArrayList();
-                while (!stack.empty()) {
+                while(!stack.isEmpty()){
                     String node = stack.pop();
                     component.add(node);
-                    for (String nei: graph.get(node)) {
-                        if (!seen.contains(nei)) {
+                    for(String nei: graph.get(node)){
+                        if(!seen.contains(nei)){
                             seen.add(nei);
                             stack.push(nei);
                         }
@@ -35,9 +34,9 @@ class Solution {
                 }
                 Collections.sort(component);
                 component.add(0, emailToName.get(email));
-                ans.add(component);
+                res.add(component);
             }
         }
-        return ans;
+        return res;
     }
 }
