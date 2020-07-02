1cd7940bde0b159c3c12544888b4dc8eb6e53a67
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-02 14:58:28 -0500

    update

--- 2020/07.02/286. Walls and Gates.java
+++ 2020/07.02/286. Walls and Gates.java
@@ -30,8 +30,10 @@ class Solution {
             }
         }
     }
-    void dfs(int[][] rooms, int i, int j, int val) {
-        if (i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] < val) return;
+    public void dfs(int[][] rooms, int i, int j, int val) {
+        if (i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length 
+        || rooms[i][j] < val) return;
+        
         rooms[i][j] = val;
         dfs(rooms, i + 1, j, val + 1);
         dfs(rooms, i - 1, j, val + 1);
