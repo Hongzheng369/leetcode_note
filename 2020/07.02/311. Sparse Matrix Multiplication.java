3e44bc87ceeeb6a959bc8159940ddbe654b94ee7
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-02 14:43:24 -0500

    initial

---
+++ 2020/07.02/311. Sparse Matrix Multiplication.java
@@ -0,0 +1,34 @@
+/*
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> res(A.size(), vector<int>(B[0].size()));
+        for (int i = 0; i < A.size(); ++i) {
+            for (int k = 0; k < A[0].size(); ++k) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < B[0].size(); ++j) {
+                        if (B[k][j] != 0) res[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+*/
+
+class Solution{
+    public int[][] multiply(int[][] A, int[][] B){
+        int[][] res = int[A.length][B[0].length];
+        for (int i = 0; i < A.length; ++i) {
+            for (int k = 0; k < A[0].length; ++k) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < B[0].length; ++j) {
+                        if (B[k][j] != 0) res[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
