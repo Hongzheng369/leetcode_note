68e16af1f5b60717d7b3a6882c431b3c583f2c7b
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-04 15:48:02 -0500

    initial

---
+++ 2020/07.04/139. Word Break.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        return helper(s, set);
+    }
+    
+
+    public boolean helper(String s, Set<String> dict) {        
+        boolean[] f = new boolean[s.length() + 1];        
+        f[0] = true;                
+        /* First DP
+        for(int i = 1; i <= s.length(); i++){
+            for(String str: dict){
+                if(str.length() <= i){
+                    if(f[i - str.length()]){
+                        if(s.substring(i-str.length(), i).equals(str)){
+                            f[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }*/
+        
+        //Second DP
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return f[s.length()];
+    }
+}
+
