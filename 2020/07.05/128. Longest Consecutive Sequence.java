ee14a3aa46126d1cdb51dba0d9d4c0ff7c5b72d7
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-05 10:37:26 -0500

    update

--- 2020/07.05/128. Longest Consecutive Sequence.java
+++ 2020/07.05/128. Longest Consecutive Sequence.java
@@ -28,56 +28,30 @@ class Solution {
 }
 
 
-class UF {
-    Map<Integer, Integer> parents;
-    Map<Integer, Integer> lengths;
-    int maxLength;
-    public UF(Set<Integer> set) {
-        parents = new HashMap<>();
-        lengths = new HashMap<>();
-        maxLength = 1;
-        for (int i : set) {
-            parents.put(i, i);
-            lengths.put(i, 1);
+public int longestConsecutive(int[] num) {
+    int res = 0;
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int n : num) {
+        if (!map.containsKey(n)) {
+            int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
+            int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+            // sum: length of the sequence n is in
+            int sum = left + right + 1;
+            map.put(n, sum);
+            
+            // keep track of the max length 
+            res = Math.max(res, sum);
+            
+            // extend the length to the boundary(s)
+            // of the sequence
+            // will do nothing if n has no neighbors
+            map.put(n - left, sum);
+            map.put(n + right, sum);
+        }
+        else {
+            // duplicates
+            continue;
         }
     }
-    public int find(int x) {
-        if (parents.get(x) == x) {
-            return x;
-        }
-        parents.put(x, find(parents.get(x)));
-        return parents.get(x);
-    }
-    public void union(int x, int y) {
-        if (find(x) == find(y)) {
-            return;
-        }
-        int parX = find(x), parY = find(y);
-        parents.put(parX, parY);
-        
-        lengths.put(parY, lengths.get(parY) + lengths.get(parX));
-        maxLength = Math.max(lengths.get(parY), maxLength);
-        return;
-    }
-}
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        Set<Integer> set = new HashSet<>();
-        for (int i : nums) {
-            set.add(i);
-        }
-        UF uf = new UF(set);
-        for (int i : set) {
-            if (set.contains(i-1)) {
-                uf.union(i-1, i);
-            }
-            if (set.contains(i+1)) {
-                uf.union(i, i+1);
-            }
-        }
-        return uf.maxLength;
-    }
+    return res;
 }
