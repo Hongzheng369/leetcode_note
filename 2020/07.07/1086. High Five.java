5aa845201f7383b1d6397176b163febf1ad0f8af
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-07 10:36:19 -0500

    initial

--- 2020/07.07/1086. High Five.java
+++ 2020/07.07/1086. High Five.java
@@ -1,3 +1,14 @@
+/*
+Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+Output: [[1,87],[2,88]]
+Explanation: 
+The average of the student with id = 1 is 87.
+The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+————————————————
+版权声明：本文为CSDN博主「YuanTheCoder」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/YuanTheCoder/java/article/details/95485292
+*/
+
 class Solution {
     public int[][] highFive(int[][] items) {   
         HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();        
