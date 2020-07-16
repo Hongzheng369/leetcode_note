7468f596fb7e8cebc5493e3a50f9a4140889d8e7
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-16 10:24:01 -0500

    initial

---
+++ 2020/07.16/1249. Minimum Remove to Make Valid Parentheses.java
@@ -0,0 +1,43 @@
+/* 
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+Example 4:
+
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+ */
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                open++;
+            }
+            else if(c == ')'){
+                if(open == 0) continue;
+                open--;
+            }
+            sb.append(c);
+        }
+        for(int i = sb.length() - 1; i >= 0 && open > 0; i--){
+            if(sb.charAt(i) == '('){
+                sb.deleteCharAt(i);
+                open--;
+            }
+        }
+        return sb.toString();
+    }
+}
