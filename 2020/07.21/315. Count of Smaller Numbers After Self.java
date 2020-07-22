8c1e017efb07d9e82bf82d745c1b137f19051aba
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-07-21 22:04:18 -0500

    update

--- 2020/07.21/315. Count of Smaller Numbers After Self.java
+++ 2020/07.21/315. Count of Smaller Numbers After Self.java
@@ -1,25 +1,16 @@
-/* You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
-
-Example:
-
-Input: [5,2,6,1]
-Output: [2,1,1,0] 
-Explanation:
-To the right of 5 there are 2 smaller elements (2 and 1).
-To the right of 2 there is only 1 smaller element (1).
-To the right of 6 there is 1 smaller element (1).
-To the right of 1 there is 0 smaller element.
- */
-
  class Solution {
-    class Node{
-        Node left;
-        Node right;
-        int val;
-        int count;
-    }
-    public Node(int val){
-        this.val = val;
+
+    
+
+
+    public List<Integer> countSmaller(int[] nums) {
+        Integer[] res = new Integer[nums.length];
+        Arrays.fill(res, 0);
+        Node root = null;
+        for(int i = nums.length - 1; i >= 0; i--){
+            root = createNode(root, new Node(nums[i]), res, i);
+        }
+        return Arrays.asList(res);
     }
     
     public Node createNode(Node root, Node node, Integer[] res, int i){
@@ -36,14 +27,15 @@ To the right of 1 there is 0 smaller element.
         }
         return root;
     }
+ }
 
-    public List<Integer> countSmaller(int[] nums) {
-        Integer[] res = new Integer[nums.length];
-        Arrays.fill(res, 0);
-        Node root = null;
-        for(int i = nums.length - 1; i >= 0; i--){
-            root = createNode(root, new Node(nums[i]), res, i);
-        }
-        return Arrays.asList(res);
+class Node{
+        Node left;
+        Node right;
+        int val;
+        int count;
+    
+    public Node(int val){
+        this.val = val;
     }
 }
