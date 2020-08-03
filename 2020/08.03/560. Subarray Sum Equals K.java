f2ad8cca3bbc9d9f24f7fe0ea887e4575046802f
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-08-03 10:22:48 -0500

    initial

---
+++ 2020/08.03/560. Subarray Sum Equals K.java
@@ -0,0 +1,13 @@
+class Solution{
+    public int subarraySum(int[] nums, int k){
+        int res = 0, int sum = 0;
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(preSum.containsKey(sum - k)) res += preSum.get(sum - k);
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+}
