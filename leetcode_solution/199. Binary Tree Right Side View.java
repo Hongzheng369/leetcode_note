a53c34a38e2dd0188d05c9b2b5ae9037d6c30a94
Hongzheng369 <hongzhengwangcn@gmail.com>
2020-08-09 21:48:50 -0500

    update

--- leetcode_solution/199. Binary Tree Right Side View.java
+++ leetcode_solution/199. Binary Tree Right Side View.java
@@ -4,21 +4,28 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
+        if(root == null)    return res;
         helper(root, res, 0);
+        //helper(root.right, res, 0)
         return res;
     }
-    public void helper(TreeNode root, List<Integer> res, int curDepth){
-        if(root == null)
-            return;
-        if(curDepth == res.size())
-            res.add(root.val);
-        helper(root.right, res, curDepth + 1);
-        helper(root.left, res, curDepth + 1);
+    
+    public void helper(TreeNode node, List<Integer> res, int curDepth){
+        if(node == null)    return;
+        if(curDepth == res.size())  res.add(node.val);
+        helper(node.right, res, curDepth + 1);
+        helper(node.left, res, curDepth + 1);
     }
 }
